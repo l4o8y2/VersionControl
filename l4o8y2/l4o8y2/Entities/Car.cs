2496762d968ac6b748aef6e6cc756fcd0241c572
l4o8y2 <dora.nagy9@stud.uni-corvinus.hu>
2022-11-04 12:23:25 +0100

    grdrzges

--- l4o8y2/l4o8y2/Entities/Car.cs
+++ l4o8y2/l4o8y2/Entities/Car.cs
@@ -1,6 +1,8 @@
 ﻿
+using l4o8y2.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +12,10 @@ namespace l4o8y2.Entities
     public class Car: Toy
     {
 
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
     }
 }
