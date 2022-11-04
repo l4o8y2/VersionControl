8ec07c349780f842d7e957055f2a8160a462e597
l4o8y2 <dora.nagy9@stud.uni-corvinus.hu>
2022-11-04 12:21:08 +0100

    vhgfh

---
+++ l4o8y2/l4o8y2/Entities/Car.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l4o8y2.Entities
+{
+    public class Car: Toy
+    {
+
+    }
+}
