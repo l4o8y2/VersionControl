7b29da56f2595716ad512f1981d8b01d06aedc4b
l4o8y2 <dora.nagy9@stud.uni-corvinus.hu>
2022-12-02 12:26:00 +0100

    2.

---
+++ l4o8y2/l4o8y2/Entities/Gender.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l4o8y2.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+    Gender ádámNeme = Gender.Male;
+    Gender évaNeme = (Gender)2;
+
+if (ádámNeme == Gender.Male)
+{
+    ...
+}
+}
